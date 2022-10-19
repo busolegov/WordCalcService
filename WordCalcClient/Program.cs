4828b17fc40c223e13022d41c563faf509e2ec9f
busolegov <n0rma.jeane@yandex.ru>
2022-10-19 22:11:12 +0300

    client_code fixed

--- WordCalcClient/Program.cs
+++ WordCalcClient/Program.cs
@@ -19,34 +19,31 @@ namespace WordCalcClient
                 client = new WordCalcService.WCServiceClient(bindingName);
                 client.Open();
 
-                while (client.State == CommunicationState.Opened)
-                {
-                    Console.WriteLine("Вы подключены к сервису WordCalc!\n");
-                    Console.WriteLine("Введите путь к файлу для подсчета слов.\n");
-                    text = File.ReadAllText(Console.ReadLine(), Encoding.UTF8);
+                Console.WriteLine("Вы подключены к сервису WordCalc!\n");
+                Console.WriteLine("Введите путь к файлу для подсчета слов.\n");
+                text = File.ReadAllText(Console.ReadLine(), Encoding.UTF8);
 
-                    if (text != null)
+                if (text != null)
+                {
+                    var mapFromService = client.WordCalculateText(text);
+                    if (mapFromService != null)
                     {
-                        var mapFromService = client.WordCalculateText(text);
-                        if (mapFromService != null)
+                        using (StreamWriter streamWriter = new StreamWriter("result.txt", false, Encoding.UTF8))
                         {
-                            using (StreamWriter streamWriter = new StreamWriter("result.txt", false, Encoding.UTF8))
+                            foreach (var item in mapFromService)
                             {
-                                foreach (var item in mapFromService)
-                                {
-                                    streamWriter.WriteLine("{0,-30} {1,5}", item.Key, item.Value);
-                                }
+                                streamWriter.WriteLine("{0,-30} {1,5}", item.Key, item.Value);
                             }
-                            Console.WriteLine("Полученный от сервиса словарь записан в файл result.txt\n");
-                            client.Close();
-                            Environment.Exit(0);
                         }
-                        else
-                        {
-                            Console.WriteLine("Ошибка получения словаря от сервиса.");
-                            client.Close();
-                            Environment.Exit(0);
-                        }
+                        Console.WriteLine("Полученный от сервиса словарь записан в файл result.txt\n");
+                        client.Close();
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка получения словаря от сервиса.");
+                        client.Close();
+                        Environment.Exit(0);
                     }
                 }
             }
